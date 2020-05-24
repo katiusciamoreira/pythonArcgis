4f8cf6d7f9f20b6ecba35b1e0c1aa46809e922b8
Katiuscia F. Moreira <64500485+katiusciamoreira@users.noreply.github.com>
2020-05-24 19:19:32 -0300

    Add files via upload

---
+++ datas/centroidCalculateField.py
@@ -0,0 +1,47 @@
+# Usar a função Calculate Field para adicionar as coordenadas x e y dos centroides de uma feição, 
+# criando estes campos na tabela de atributos da feição desejada
+# Válido para arquivos classes de feição do tipo poligono, linha e ponto..etc)
+# Obs.: No caso de pontos as coordenadas centrais adicionadas coincidirão com as mesmas da localização do ponto
+
+# Importar os módulos
+import arcpy
+from arcpy import env
+
+# Para capturar os parametros de entrada e saida da interface
+input_folder = arcpy.GetParameterAsText(0)
+output_folder = arcpy.GetParameterAsText(1)
+
+# Para listar os shapefiles da pasta de entrada
+arcpy.env.workspace = input_folder
+listaFC = arcpy.ListFeatureClasses("*.shp")
+
+# Caso nao sejam encontrados os arquivos Shapefiles, emite-se um aviso
+if len(listaFC) == 0:
+    arcpy.AddWarning("Not found shapefiles in " + input_folder)
+
+# Para cada Feature Class na lista do input, adicionar as coordenadas dos centroids na tabela de atributos
+for file in listaFC:
+    try: 
+        env.workspace = input_folder    
+    # Criar variaveis
+        fieldName1 = "xCentroid"
+        fieldName2 = "yCentroid"
+        fieldPrecision = 18
+        fieldScale = 11
+ 
+    # Adicionar campos
+        arcpy.AddField_management(file, fieldName1, "DOUBLE", fieldPrecision, fieldScale)
+        arcpy.AddField_management(file, fieldName2, "DOUBLE", fieldPrecision, fieldScale)
+ 
+    # Calcular centroide
+        arcpy.CalculateField_management(file, fieldName1, "!SHAPE.CENTROID.X!", "PYTHON_9.3")
+        arcpy.CalculateField_management(file, fieldName2, "!SHAPE.CENTROID.Y!", "PYTHON_9.3")
+    except Exception as e:
+    # Se um erro ocorrer, imprimir o numero da linha e a mensagem de erro
+        import traceback
+        import sys
+        tb = sys.exc_info()[2]
+        print("Line {0}".format(tb.tb_lineno))
+        print(e.message)
+
+# Para este codigo usei algumas bases da documentação do arcgis disponivel em: http://resources.arcgis.com/en/help/main/10.1/index.html#//00170000004m000000
